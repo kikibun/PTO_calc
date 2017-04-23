f9a1cfca5c40c064db52945f4ef22d77345960c6
Kirstin <kirstinalexis@gmail.com>
2017-04-22 21:50:36 -0500

    Add files via upload

---
+++ PTO Calculator.py
@@ -0,0 +1,10 @@
+#PTO Calculator
+
+# add PTO to bank - accrue 7.16 hours every 2 weeks
+# given a date, tally hours accrued since date (possible_PTO)
+
+# subtract PTO manually
+# given days of PTO desired, remove PTO hours (8 hrs / day) (taken_PTO)
+
+# return leftover PTO hours
+# use possible_PTO and taken_PTO to determine remaining_PTO
